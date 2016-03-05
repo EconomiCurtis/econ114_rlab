5bdf1f820e43d76ef9c319d8032479c32a23cb9a
economicurtis <curtisesjunk@gmail.com>
2016-03-04 17:06:11 -0800

    startup ps4

---
+++ ps4.r
@@ -0,0 +1,71 @@
+df <- read.table(
+  "C:/Users/OKComputer/Dropbox/R Lab/Econ114_RLab/Econ114_Rlab/guessTheModel1.txt", 
+  quote="\"", comment.char="")
+
+
+df$V1
+df$x = 1:100000
+
+
+library(ggplot2)
+
+ggplot(
+  df, 
+  aes(x = x,
+      y = V1)
+)  +
+  geom_point(alpha = 0.05)
+
+ggplot(
+  df, 
+  aes(x = x,
+      y = V1)
+)  +
+  geom_point(alpha = 0.05) + 
+  stat_smooth()
+
+reg.1 <- lm(V1 ~ x, data = df)
+reg.1 <- glm(V1 ~ x, data = df, )
+summary(reg.1)
+
+
+ggplot(
+  df, 
+  aes(x = V1)
+)  +
+  geom_histogram(bins = 60)
+
+summary(df$V1)
+
+#-----------------------------------------
+library(zoo)
+
+df.ts <- ts(df$V1)
+plot(df.ts)
+
+reg.3 <- ar(df.ts, method = "mle")
+summary(reg.3)
+reg.3
+
+
+
+ar.ols(df.ts)
+
+
+
+
+
+reg.4 <- arima(df.ts)
+summary(reg.4)
+
+
+# ------------
+df$V1CumSum <- cumsum(df$V1)
+ggplot(
+  df, 
+  aes(x = x,
+      y = V1CumSum)
+) + 
+  geom_line()
+
+acf(df$V1, 50)
